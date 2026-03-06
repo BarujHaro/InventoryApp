8590b80befd327db55d3c9c0b375d484bae010b9
BarujHaro <fernando.haro.sal@gmail.com>
2026-03-06 13:18:22 -0600

    db pool modified

--- src/models/database.js
+++ src/models/database.js
@@ -1,13 +1,28 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
-});
+let pool;
+
+if (process.env.DATABASE_URL){
+
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+
+}else{
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432,
+  });
+}
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
